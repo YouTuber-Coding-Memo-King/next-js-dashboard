11702c2931287b3d9eca7349d608dbd7b8508724
hongokim2017@gmail.com <hongokim2017@gmail.com>
2023-12-30 20:03:55 +0900

    chapter13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -38,10 +38,19 @@ export async function createInvoice(formData: FormData) {
     
     console.log('date type', typeof date)
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+    try{
+        
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+
+    }catch(error){
+        return {
+            message: 'Database Error: Failed to Create Invoice.'
+        }
+    }
+
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -57,17 +66,32 @@ export async function updateInvoice (id : string, formData: FormData){
         }
     );
     const amountInCents = amount * 100;
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount=${amountInCents},
-        status=${status} WHERE id = ${id}`;
+
+    try{        
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount=${amountInCents},
+            status=${status} WHERE id = ${id}`;
+    }catch (error){
+        return{
+            message: 'Database error: Failed to update Invoice.'
+        }
+    }
 
         revalidatePath('/dashboard/invoces');
         redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice(id: string) {
-    await sql `DELETE FROM invoices WHERE id=${id}`;
-    revalidatePath('/dashboard/invoices')
-}
+    throw new Error('Failed to Delete Invoice');
+   
+    // Unreachable code block
+    try {
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+      revalidatePath('/dashboard/invoices');
+      return { message: 'Deleted Invoice' };
+    } catch (error) {
+      return { message: 'Database Error: Failed to Delete Invoice' };
+    }
+  }
 
